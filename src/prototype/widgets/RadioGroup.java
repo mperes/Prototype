4c7fb7da860aa8edde949914a2e3bbb3e62606a2
Miguel Peres <m.peres@gmail.com>
2011-12-22 17:30:30 +0100

    RadioGroup

---
+++ src/prototype/widgets/RadioGroup.java
@@ -0,0 +1,14 @@
+package prototype.widgets;
+
+import prototype.Part;
+
+public class RadioGroup extends Part {
+	
+	public RadioGroup(int width, int margin, String...strings) {
+		super();
+		for(String label : strings) {
+			
+		}
+	}
+
+}
